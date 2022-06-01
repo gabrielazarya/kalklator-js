57d63adc90e1981d9b8b6603b65df98095aad583
Gabriel Azarya <gabrielkrishna5@gmail.com>
2022-06-01 17:57:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+//insert angkanya
+function insert(num) {
+    document.form.textarea.value = document.form.textarea.value + num;
+}
+
+//hitung
+function hasil() {
+    var hasils = document.form.textarea.value;
+    document.form.textarea.value = eval(hasils);
+    
+}
+
+//clean
+function clean() {
+    document.form.textarea.value = "";
+}
+
+//hapus
+function hapus() {
+    var hasils = document.form.textarea.value;
+    document.form.textarea.value = hasils.substring(0,hasils.length-1);
+}
+
